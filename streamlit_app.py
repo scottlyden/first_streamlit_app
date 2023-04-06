a52230d56915b6b77df22aa2fc3190d9623fbb14
scottlyden <scottlyden@yahoo.com>
2023-04-06 12:41:25 -0700

    Update streamlit_app.py
    Insert flow-control stop before db insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # render as dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
