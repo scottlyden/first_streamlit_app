1cbbb6b68d17ca387002ad0ea407e91bbf2d7578
scottlyden <scottlyden@yahoo.com>
2023-04-06 12:28:23 -0700

    Update streamlit_app.py
    Let user add a fruit to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,6 @@ my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+streamlit.text(f'Thanks for adding {fruit_choice}')
