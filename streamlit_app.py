6b5dea5d361f2a82a20b654504459ef83b1990d0
scottlyden <scottlyden@yahoo.com>
2023-04-06 13:48:31 -0700

    Update streamlit_app.py
    Finally make the db insert use the user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def get_fruit_load_list():
     
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+        my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values('{new_fruit}')")
         return f"Thanks for adding {new_fruit}"
 
 streamlit.title('Healthy Diner')
