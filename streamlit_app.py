526484365e21f90b873b333a7846ff506be32340
scottlyden <scottlyden@yahoo.com>
2023-04-06 13:43:41 -0700

    Update streamlit_app.py
    Move db insert to function insert_row_snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,11 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
         return my_cur.fetchall()
+    
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+        return f"Thanks for adding {new_fruit}"
 
 streamlit.title('Healthy Diner')
 
@@ -55,8 +60,12 @@ if streamlit.button("Fruit to load"):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.text(f'Thanks for adding {fruit_choice}')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+
+if streamlit.button('Add a fruit'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
 # This should fail
 # my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
